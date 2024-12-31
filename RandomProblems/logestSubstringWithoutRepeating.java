beb3b5e8d12801f619777d44c0ecfefde80cb638
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2024-12-31 23:12:05 +0530

    Create logestSubstringWithoutRepeating.java

---
+++ RandomProblems/logestSubstringWithoutRepeating.java
@@ -0,0 +1,59 @@
+/*
+Longest Substring Without Repeating Characters
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example 1:
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+*/
+
+// Method 1:
+public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int start = 0;
+
+        Set<Character> hset = new HashSet<>();
+
+        for(int end = 0; end <s.length(); end++) {
+            while(hset.contains(s.charAt(end))) {
+                hset.remove(s.charAt(start));
+                start++;
+            }
+            hset.add(s.charAt(end));
+            max = Math.max(max, end - start + 1);
+        }
+        return max;
+    }
+
+// Method 2: 
+public int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        int start = 0;
+
+        Set<Character> hset = new HashSet<>();
+
+        for(int end = 0; end <s.length(); end++) {
+            if(hset.contains(s.charAt(end))) {
+                hset.remove(s.charAt(start));
+                start++;
+                end--;
+                continue;
+            }
+            
+            hset.add(s.charAt(end));
+            max = Math.max(max, end - start + 1);
+        }
+        return max;
+    }

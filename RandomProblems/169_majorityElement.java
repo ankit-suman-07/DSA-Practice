66421a0afcd0c8fe41a402383ebc2c3e3f6a8151
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-01-02 23:27:18 +0530

    Update 169_majorityElement.java

--- RandomProblems/169_majorityElement.java
+++ RandomProblems/169_majorityElement.java
@@ -12,7 +12,7 @@ Input: nums = [2,2,1,1,1,2,2]
 Output: 2
 */
 
-// Method 1: 
+// Method 1: (Boyer-Moore Voting Algorithm)
 public int majorityElement(int[] nums) {
         int candidate = nums[0];
         int count = 0;
@@ -28,6 +28,49 @@ public int majorityElement(int[] nums) {
         return candidate; 
     }
 
-// Method 2:
+// Method 2: (Bit-Manipulation)
+public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int majority = 0;
+
+        for (int bit = 0; bit < 32; bit++) {
+            int bitCount = 0; 
+
+            for (int num : nums) {
+                if ((num >> bit & 1) == 1) {
+                    bitCount++;
+                }
+            }
+
+            if (bitCount > n / 2) {
+                majority |= (1 << bit);
+            }
+        }
+
+        return majority;
+    }
 
 // Method 3:
+public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++) {
+            if(hmap.containsKey(nums[i])) {
+                hmap.put(nums[i], hmap.get(nums[i]) + 1);
+            } else {
+                hmap.put(nums[i],1);
+            }
+        }
+
+        int max = nums[0];
+        int maxCount = 1;
+
+        for(int keys: hmap.keySet()) {
+            if(maxCount < hmap.get(keys)) {
+                max = keys;
+                maxCount = hmap.get(keys);
+            }
+        }
+
+        return max;
+    }

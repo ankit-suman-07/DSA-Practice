f892c7de28bd679c630fab8eb130719d8e1c620a
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-01-03 00:03:55 +0530

    Create 228_summaryRanges.java

---
+++ RandomProblems/228_summaryRanges.java
@@ -0,0 +1,67 @@
+/*
+228. Summary Ranges
+You are given a sorted unique integer array nums.
+A range [a,b] is the set of all integers from a to b (inclusive).
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b
+ 
+Example 1:
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+
+Example 2:
+Input: nums = [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+Explanation: The ranges are:
+[0,0] --> "0"
+[2,4] --> "2->4"
+[6,6] --> "6"
+[8,9] --> "8->9"
+*/
+
+// Method 1:
+
+public List<String> summaryRanges(int[] nums) {
+        List<String> list = new ArrayList<>();
+        if (nums.length == 0) return list;
+
+        int start = nums[0];
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (i == nums.length || nums[i] - nums[i - 1] != 1) {
+                if (start == nums[i - 1]) {
+                    list.add(String.valueOf(start));
+                } else {
+                    list.add(start + "->" + nums[i - 1]);
+                }
+                if (i < nums.length) {
+                    start = nums[i];
+                }
+            }
+        }
+        return list;
+    }
+
+// Method 2:
+public List<String> summaryRanges(int[] nums) {
+        List<String> ans = new ArrayList<>();
+        for (int i = 0, j, n = nums.length; i < n; i = j + 1) {
+            j = i;
+            while (j + 1 < n && nums[j + 1] == nums[j] + 1) {
+                ++j;
+            }
+            ans.add(f(nums, i, j));
+        }
+        return ans;
+    }
+
+    private String f(int[] nums, int i, int j) {
+        return i == j ? nums[i] + "" : String.format("%d->%d", nums[i], nums[j]);
+    }
